f9a648358c37cd8128a93f9f6b3d9af0ed561346
Erik Molina <90419030+Biacuya@users.noreply.github.com>
2023-11-19 02:20:27 -0500

    Funcional
    El Algoritmo TSP funciona con el objeto address

--- src/models/app_flow.py
+++ src/models/app_flow.py
@@ -1,5 +1,6 @@
 from address import Address
 from map import obtener_coordenadas, get_distance
+from graph import tsp
 
 
 class AppFlow:
@@ -40,36 +41,42 @@ class AppFlow:
             dictionary_distances = {
                 self.list_address[count_actual_position].get_name_address(): {}
             }
-            for address_coordinates in self.list_address:
-                print(f"Count: {count_actual_position}")
-                print(f"Count2: {count_leng_list}")
-                if count_leng_list < len(self.list_address):
-                    aux = get_distance(
-                        self.list_address[count_actual_position].get_coordinates(),
-                        address_coordinates.get_coordinates(),
-                    )
-                    raw_time_between_address = aux / self.SPEED
-                    time_in_minutes_between_address = round(
-                        (raw_time_between_address * 60), 2
-                    )
-                    count_leng_list += 1
-                    dictionary_distances[
-                        self.list_address[count_actual_position].get_name_address()
-                    ][
-                        address_coordinates.get_name_address()
-                    ] = time_in_minutes_between_address
-                    self.list_address[count_actual_position].set_dictionary_of_address(
-                        dictionary_distances
-                    )
-                    print(
+            for j, address_time in enumerate(self.list_time_between_scholl):
+                for address_coordinates in self.list_address:
+                    print(f"Count: {count_actual_position}")
+                    print(f"Count2: {count_leng_list}")
+                    if count_leng_list < len(self.list_address):
+                        aux = get_distance(
+                            self.list_address[count_actual_position].get_coordinates(),
+                            address_coordinates.get_coordinates(),
+                        )
+                        raw_time_between_address = aux / self.SPEED
+                        time_in_minutes_between_address = round(
+                            (raw_time_between_address * 60), 2
+                        )
+                        count_leng_list += 1
+                        dictionary_distances[
+                            self.list_address[count_actual_position].get_name_address()
+                        ][
+                            address_coordinates.get_name_address()
+                        ] = time_in_minutes_between_address
+                        dictionary_distances[
+                            self.list_address[count_actual_position].get_name_address()
+                        ]["scholl"] = self.list_time_between_scholl[
+                            count_actual_position
+                        ]
                         self.list_address[
                             count_actual_position
-                        ].get_dictionary_of_address()
-                    )
-                    print("Metodo direcciones: ", dictionary_distances)
-                else:
-                    print("Acabo y se reinicia")
-                    count_leng_list = 0
+                        ].set_dictionary_of_address(dictionary_distances)
+                        print(
+                            self.list_address[
+                                count_actual_position
+                            ].get_dictionary_of_address()
+                        )
+                        # print("Metodo direcciones: ", dictionary_distances)
+                    else:
+                        print("Acabo y se reinicia")
+                        count_leng_list = 0
             count_actual_position += 1
 
     def calculate_time(self):
@@ -111,12 +118,25 @@ class AppFlow:
             )
 
     def dictionary_graph(self):
-        graph = {"scholl": {}}
-        for name_test in self.list_completed_address:
-            graph["scholl"][
-                name_test.get_name_address()
-            ] = name_test.get_time_list_address()
-        print(graph)
+        graph_scholl = {"scholl": {}}
+
+        for name_address_graph in self.list_completed_address:
+            graph_scholl["scholl"][
+                name_address_graph.get_name_address()
+            ] = name_address_graph.get_time_list_address()
+            graph = name_address_graph.get_dictionary_of_address()
+
+            for key, value in graph.items():
+                if key not in graph_scholl:
+                    graph_scholl[key] = {}
+                graph_scholl[key].update(value)
+
+        print(graph_scholl)
+        return graph_scholl
+
+    def creation_tsp(self, graph):
+        route = tsp(graph)
+        print(f"ruta optima{route}")
 
 
 app = AppFlow()
@@ -130,8 +150,9 @@ app.create_address("Calle 13, Tunja, Boyaca, Colombia")
 app.create_address("Los Hongos, Tunja, Boyaca, Colombia")
 # app.create_address("Centro Comercial Unicentro Tunja, Tunja, Boyaca, Colombia")
 app.get_distance_between_addresses_and_scholl_address()
-app.get_distance_between_addresses()
 app.calculate_time()
+app.get_distance_between_addresses()
 app.create_completed_address()
 app.print_aux()
-app.dictionary_graph()
+# app.dictionary_graph()
+app.creation_tsp(app.dictionary_graph())
